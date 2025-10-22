b7893bba88fa87e158ec4ce57a74751c7c38740d
topher235 <topherhurt@gmail.com>
2025-10-22 13:06:22 -0400

    Convert orb from square to circle

--- scenes/collectibles/orb.gd
+++ scenes/collectibles/orb.gd
@@ -4,8 +4,8 @@ extends BaseCollectible
 @export var orb_color: Color = Color(0, 1, 1, 1)  # Cyan
 @export var orb_radius: float = 15.0
 
+var polygon_node: Polygon2D = null
 
-#@onready var visual: ColorRect = $Visual
 
 func _ready() -> void:
     super._ready()
@@ -16,9 +16,28 @@ func _ready() -> void:
 
 func _setup_visual() -> void:
     if visual:
-        visual.size = Vector2(orb_radius * 2, orb_radius * 2)
-        visual.position = -visual.size / 2
-        visual.color = orb_color
+        # Create circular shape using Polygon2D
+        var circle_polygon = Polygon2D.new()
+
+        # Generate circle points
+        var num_points = 32
+        var points = PackedVector2Array()
+
+        for i in range(num_points):
+            var angle = (float(i) / num_points) * TAU
+            var x = cos(angle) * orb_radius
+            var y = sin(angle) * orb_radius
+            points.append(Vector2(x, y))
+
+        circle_polygon.polygon = points
+        circle_polygon.color = orb_color
+
+        # Remove the ColorRect visual
+        visual.queue_free()
+
+        # Add circle as a child
+        add_child(circle_polygon)
+        polygon_node = circle_polygon
 
 
 func _setup_collision() -> void:
